189b1f0d39b7082c0c1cac813b62bae0746fd7b9
osirvics <kedvic@gmail.com>
2023-01-14 10:23:52 +0100

    change login details

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ p3 = os.environ['p3']
 p4 = os.environ['p4']
 p5 = os.environ['p5']
 
-usernames = [u1, u2, u3, u4, u5]
-passwords = [p1, p2, p3, p4, p5]
+usernames = [u3, u4, u5]
+passwords = [p3, p4, p5]
 
 claimed = 0
 
